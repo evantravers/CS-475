ffbec8741a3321c1aa051aea0b8ca012a849ea51
evantravers <widgetwebworks@gmail.com>
2011-01-12 12:33:08 -0600

    I CAN SEE CLOUDS

--- hw0/main.cpp
+++ hw0/main.cpp
@@ -13,6 +13,178 @@
 
 using namespace std;
 
+//Represents a terrain, by storing a set of heights and normals at 2D locations
+class Terrain {
+	private:
+		int w; //Width
+		int l; //Length
+		float** hs; //Heights
+		Vec3f** normals;
+		bool computedNormals; //Whether normals is up-to-date
+	public:
+		Terrain(int w2, int l2) {
+			w = w2;
+			l = l2;
+			
+			hs = new float*[l];
+			for(int i = 0; i < l; i++) {
+				hs[i] = new float[w];
+			}
+			
+			normals = new Vec3f*[l];
+			for(int i = 0; i < l; i++) {
+				normals[i] = new Vec3f[w];
+			}
+			
+			computedNormals = false;
+		}
+		
+		~Terrain() {
+			for(int i = 0; i < l; i++) {
+				delete[] hs[i];
+			}
+			delete[] hs;
+			
+			for(int i = 0; i < l; i++) {
+				delete[] normals[i];
+			}
+			delete[] normals;
+		}
+		
+		int width() {
+			return w;
+		}
+		
+		int length() {
+			return l;
+		}
+		
+		//Sets the height at (x, z) to y
+		void setHeight(int x, int z, float y) {
+			hs[z][x] = y;
+			computedNormals = false;
+		}
+		
+		//Returns the height at (x, z)
+		float getHeight(int x, int z) {
+			return hs[z][x];
+		}
+		
+		//Computes the normals, if they haven't been computed yet
+		void computeNormals() {
+			if (computedNormals) {
+				return;
+			}
+			
+			//Compute the rough version of the normals
+			Vec3f** normals2 = new Vec3f*[l];
+			for(int i = 0; i < l; i++) {
+				normals2[i] = new Vec3f[w];
+			}
+			
+			for(int z = 0; z < l; z++) {
+				for(int x = 0; x < w; x++) {
+					Vec3f sum(0.0f, 0.0f, 0.0f);
+					
+					Vec3f out;
+					if (z > 0) {
+						out = Vec3f(0.0f, hs[z - 1][x] - hs[z][x], -1.0f);
+					}
+					Vec3f in;
+					if (z < l - 1) {
+						in = Vec3f(0.0f, hs[z + 1][x] - hs[z][x], 1.0f);
+					}
+					Vec3f left;
+					if (x > 0) {
+						left = Vec3f(-1.0f, hs[z][x - 1] - hs[z][x], 0.0f);
+					}
+					Vec3f right;
+					if (x < w - 1) {
+						right = Vec3f(1.0f, hs[z][x + 1] - hs[z][x], 0.0f);
+					}
+					
+					if (x > 0 && z > 0) {
+						sum += out.cross(left).normalize();
+					}
+					if (x > 0 && z < l - 1) {
+						sum += left.cross(in).normalize();
+					}
+					if (x < w - 1 && z < l - 1) {
+						sum += in.cross(right).normalize();
+					}
+					if (x < w - 1 && z > 0) {
+						sum += right.cross(out).normalize();
+					}
+					
+					normals2[z][x] = sum;
+				}
+			}
+			
+			//Smooth out the normals
+			const float FALLOUT_RATIO = 0.5f;
+			for(int z = 0; z < l; z++) {
+				for(int x = 0; x < w; x++) {
+					Vec3f sum = normals2[z][x];
+					
+					if (x > 0) {
+						sum += normals2[z][x - 1] * FALLOUT_RATIO;
+					}
+					if (x < w - 1) {
+						sum += normals2[z][x + 1] * FALLOUT_RATIO;
+					}
+					if (z > 0) {
+						sum += normals2[z - 1][x] * FALLOUT_RATIO;
+					}
+					if (z < l - 1) {
+						sum += normals2[z + 1][x] * FALLOUT_RATIO;
+					}
+					
+					if (sum.magnitude() == 0) {
+						sum = Vec3f(0.0f, 1.0f, 0.0f);
+					}
+					normals[z][x] = sum;
+				}
+			}
+			
+			for(int i = 0; i < l; i++) {
+				delete[] normals2[i];
+			}
+			delete[] normals2;
+			
+			computedNormals = true;
+		}
+		
+		//Returns the normal at (x, z)
+		Vec3f getNormal(int x, int z) {
+			if (!computedNormals) {
+				computeNormals();
+			}
+			return normals[z][x];
+		}
+};
+
+//Loads a terrain from a heightmap.  The heights of the terrain range from
+//-height / 2 to height / 2.
+Terrain* loadTerrain(const char* filename, float height) {
+	Image* image = loadBMP(filename);
+	Terrain* t = new Terrain(image->width, image->height);
+	for(int y = 0; y < image->height; y++) {
+		for(int x = 0; x < image->width; x++) {
+			unsigned char color =
+				(unsigned char)image->pixels[3 * (y * image->width + x)];
+			float h = height * ((color / 255.0f) - 0.5f);
+			t->setHeight(x, y, h);
+		}
+	}
+	
+	delete image;
+	t->computeNormals();
+	return t;
+}
+
+float _angle = 60.0f;
+Terrain* _terrain;
+
 void cleanup() {
 	delete _terrain;
 }
@@ -24,7 +196,6 @@ void handleKeypress(unsigned char key, int x, int y) {
 			exit(0);
 	}
 }
-
 void initRendering() {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_COLOR_MATERIAL);
@@ -65,6 +236,9 @@ void drawScene() {
 				 -(float)(_terrain->length() - 1) / 2);
 	
 	glColor3f(0.3f, 0.9f, 0.0f);
+	GLdouble cutplane[] = {0.f, 1.f, 0.f, 9.f};
+	glClipPlane(GL_CLIP_PLANE0, cutplane);
+	glEnable(GL_CLIP_PLANE0);
 	for(int z = 0; z < _terrain->length() - 1; z++) {
 		//Makes OpenGL draw a triangle at every three consecutive vertices
 		glBegin(GL_TRIANGLE_STRIP);
@@ -97,7 +271,7 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(800, 800);
 	
-	glutCreateWindow("WINTER STOMR ALERT");
+	glutCreateWindow("WINTER STORM ALERT");
 	initRendering();
 	
 	_terrain = loadTerrain("heightmap2.bmp", 20);
