253cd8908d2176d1d0ec5b5282fe784a4edc9fea
Evan Travers <widgetwebworks@gmail.com>
2011-01-22 12:28:28 -0600

    homework 1

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <fstream>
 #include <stdlib.h> //Needed for "exit" function
 
@@ -11,6 +13,8 @@
 #endif
 
 using namespace std;
+vector<float> v;
+float val;
 
 //Called when a key is pressed
 void handleKeypress(unsigned char key, //The key that was pressed
@@ -20,7 +24,6 @@ void handleKeypress(unsigned char key, //The key that was pressed
 			exit(0); //Exit the program
 	}
 }
-
 //Initializes 3D rendering
 void initRendering() {
 	//Makes 3D drawing work when something is in front of something else
@@ -39,7 +42,7 @@ void handleResize(int w, int h) {
 	gluPerspective(45.0,                  //The camera angle
 				   (double)w / (double)h, //The width-to-height ratio
 				   1.0,                   //The near z clipping coordinate
-				   200.0);                //The far z clipping coordinate
+				   900.0);                //The far z clipping coordinate
 }
 
 //Draws the 3D scene
@@ -48,51 +51,57 @@ void drawScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+
+  glPushMatrix();
+  glTranslatef(0.f, 0.f, 5.f);
+
 	glLoadIdentity(); //Reset the drawing perspective
 	
-	glBegin(GL_QUADS); //Begin quadrilateral coordinates
-	
-	//Trapezoid
-	glVertex3f(-0.7f, -1.5f, -5.0f);
-	glVertex3f(0.7f, -1.5f, -5.0f);
-	glVertex3f(0.4f, -0.5f, -5.0f);
-	glVertex3f(-0.4f, -0.5f, -5.0f);
-	
+	glBegin(GL_QUADS); 
+  int i;
+  glVertex3f(0.f, 0.f, 1.f);
+  glVertex3f(1.f, 0.f, 1.f);
+  glVertex3f(0.f, 1.f, 1.f);
+  glVertex3f(1.f, 1.f, 1.f);
+  /*for (i = 0; i < ((int) v.size())-1; i++) {
+    glVertex3f((float) i, v.at(i), v.at(i+1));
+  }*/
 	glEnd(); //End quadrilateral coordinates
 	
-	glBegin(GL_TRIANGLES); //Begin triangle coordinates
-	
-	//Pentagon
-	glVertex3f(0.5f, 0.5f, -5.0f);
-	glVertex3f(1.5f, 0.5f, -5.0f);
-	glVertex3f(0.5f, 1.0f, -5.0f);
-	
-	glVertex3f(0.5f, 1.0f, -5.0f);
-	glVertex3f(1.5f, 0.5f, -5.0f);
-	glVertex3f(1.5f, 1.0f, -5.0f);
-	
-	glVertex3f(0.5f, 1.0f, -5.0f);
-	glVertex3f(1.5f, 1.0f, -5.0f);
-	glVertex3f(1.0f, 1.5f, -5.0f);
-	
-	//Triangle
-	glVertex3f(-0.5f, 0.5f, -5.0f);
-	glVertex3f(-1.0f, 1.5f, -5.0f);
-	glVertex3f(-1.5f, 0.5f, -5.0f);
-	
-	glEnd(); //End triangle coordinates
-	
+  glPopMatrix();
 	glutSwapBuffers(); //Send the 3D scene to the screen
 }
 
 int main(int argc, char** argv) {
+  // read in files
+  
+  // config file
+  fstream config;
+  config.open("config.ini");
+  config >> val;
+  config.close();
+
+  // input
+  float temp;
+  fstream input;
+  input.open("input.txt");
+  string line;
+  getline(input, line);
+  while (!input.eof()) {
+    input >> temp;
+    // i am currently dividing by 100, may not choose to do that
+    temp = temp/100;
+    v.push_back(temp);
+    printf("%f, %i\n", temp, (int)v.size());
+  }
+
 	//Initialize GLUT
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(400, 400); //Set the window size
 	
 	//Create the window
-	glutCreateWindow("Basic Shapes - videotutorialsrock.com");
+	glutCreateWindow("hw1");
 	initRendering(); //Initialize rendering
 	
 	//Set handler functions for drawing, keypresses, and window resizes
