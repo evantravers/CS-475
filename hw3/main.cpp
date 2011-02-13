2d5ceae795f187b6aacec007135892fc79dc0f36
Evan Travers <widgetwebworks@gmail.com>
2011-02-12 21:11:57 -0600

    adding some more cheese to teh mix

---
+++ hw3/main.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "imageloader.h"
+#include "vec3f.h"
+
+using namespace std;
+
+// for storing vertexes
+
+
+float _angle = 30.0f;
+
+void cleanup() {
+}
+
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+		case 27: //Escape key
+			cleanup();
+			exit(0);
+	}
+}
+void initRendering() {
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glShadeModel(GL_SMOOTH);
+}
+
+void handleResize(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(50.0, (double)w / (double)h, 1.0, 900.0);
+}
+
+void drawScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(1.0f, 0.0f, -10.0f);
+	glRotatef(20.0f, 3.0f, 0.0f, 0.0f);
+	glRotatef(-_angle, 0.0f, 1.0f, 0.0f);
+	
+	GLfloat ambientColor[] = {0.4f, 0.4f, 0.4f, 1.0f};
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+	
+	GLfloat lightColor0[] = {0.6f, 0.6f, 0.6f, 1.0f};
+	GLfloat lightPos0[] = {-0.5f, 0.8f, 0.1f, 0.0f};
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
+  // this is where you should draw your objects
+  // there had better be code here by tomorrow or I will make you sad.
+  // I should take the eggMesh that I hopefully created, consisting of two solid objects 
+  // derived from the slices of the egg I took 
+  glBegin(GL_POINTS);
+    // crawl the images
+    char filename[21];
+    int i=1;
+    // this is where you specify the number of slices
+    for (i = 1; i < 5; i++) {
+      sprintf(filename, "data/%d.bmp",i);
+      Image* image;
+      image = loadBMP(filename);
+      int width = image->width;
+      int height = image->height;
+      int y = i;
+      int x, z;
+      for (x = 0; x < width; x++) {
+        for (z = 0; z < height; z++) {
+          unsigned char redcolor = 0;
+          unsigned char bluecolor = 0;
+          redcolor = (unsigned char)image ->pixels[3*(z * image->width + x)+2];
+          // unsigned char redcolor = (unsigned char)image ->pixels[i];
+          // unsigned char greencolor = (unsigned char)image ->pixels[3*(z * image->width + x)+1];
+          bluecolor = (unsigned char)image ->pixels[3*(z * image->width + x)];
+          // printf("%f, %f, %f\n", (float)redcolor/255.0f, (float)greencolor/255.0f, (float)bluecolor/255.0f);
+          // if the color is close to white, then draw egg
+          // if red is really low, then background
+          // if red is high, and blue is high, then egg
+          // if red is high, and blue is around 100, then yolk
+          int red = (int)redcolor;
+          int blue = (int)bluecolor;
+          if (red<190) {
+            // then we are inside the cheese
+            glVertex3f((float)x/100.0f, (float)y/4.f, (float)z/100.0f);
+          }
+          else {
+            // glColor3f(0.0f, 0.0f, 0.9f);
+            // glVertex3i(x, y*12, z);
+          }  
+        }
+      }
+    }
+  glEnd();
+
+	glutSwapBuffers();
+}
+
+void update(int value) {
+	_angle += .5f;
+	if (_angle > 360) {
+		_angle -= 360;
+	}
+	
+	glutPostRedisplay();
+	glutTimerFunc(25, update, 0);
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 800);
+	
+	glutCreateWindow("cheese smells funny");
+	initRendering();
+	
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+	glutTimerFunc(25, update, 0);
+	
+	glutMainLoop();
+	return 0;
+}
+
