48e885c7f3f5e923263219b829d30e5a133f5ea3
Evan Travers <widgetwebworks@gmail.com>
2011-02-17 09:09:52 -0600

    working on the datatype, trying to use 3d vectors, currently doesn't compile

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <vector>
 
 #ifdef __APPLE__
 #include <OpenGL/OpenGL.h>
@@ -14,6 +15,7 @@
 #include "vec3f.h"
 
 using namespace std;
+using std::vector;
 
 // for storing vertexes
 
@@ -23,6 +25,10 @@ float _angleLon = 0.0f;
 float _walk = -10.0f;
 float _stride = 0.0f;
 float _elevate = 0.0f;
+static int width;
+static int height;
+static vector<vector<vector<int> > > voxels;
+static int sample=1;
 
 void cleanup() {
 }
@@ -133,44 +139,32 @@ void drawScene() {
   // derived from the slices of the cheese I took 
   glBegin(GL_POINTS);
     // crawl the images
-    char filename[21];
-    int i=1;
-    int range = 190;
-    int sample = 1;
-    // this is where you specify the number of slices
-    for (i = 1; i < 5; i++) {
-      sprintf(filename, "data/blurry/%d.bmp",i);
-      Image* image;
-      image = loadBMP(filename);
-      int width = image->width;
-      int height = image->height;
-      int y = i;
-      int x, z;
-      int target=1;
-      for (x = 0; x < width; x+=sample) {
-        for (z = 0; z < height; z+=sample) {
-          unsigned char redcolor = 0;
-          redcolor = (unsigned char)image ->pixels[3*(z * image->width + x)+2];
-          int red = (int)redcolor;
-          float xb = (float)(x-(width/2))/100.0f;
-          float yb = (float)y/7.0f;
-          float zb = (float)(z- (height/2))/100.0f;
-          // time to outline the cheese
-          if (target) {
-            if (red<range) {
-              glVertex3f(xb, yb, zb);
-              target=0;
-            }
-          }
-          else {
-            if (red>range) {
-              glVertex3f(xb, yb, zb);
-              target=1;
-            }
-          }
-        }
-      }
-    }
+  int i = 1;
+  for (i = 1; i < 5; i++) {
+    int y = i;
+    int x, z;
+    int target=1;
+    for (x = 0; x < width; x+=sample) {
+     for (z = 0; z < height; z+=sample) {
+       float xb = (float)(x-(width/2))/100.0f;
+       float yb = (float)y/7.0f;
+       float zb = (float)(z- (height/2))/100.0f;
+       // time to outline the cheese
+       if (target) {
+         if (voxels[x][y][z]>0) {
+           glVertex3f(xb, yb, zb);
+           target=0;
+         }
+       }
+       else {
+         if (voxels[x][y][z]==0) {
+           glVertex3f(xb, yb, zb);
+           target=1;
+         }
+       }
+     }
+   }
+  }
   glEnd();
 
 	glutSwapBuffers();
@@ -187,6 +181,49 @@ void update(int value) {
 }
 
 int main(int argc, char** argv) {
+  // build the datatype
+  char filename[21];
+  int i=1;
+  int range = 100;
+  // this is where you specify the number of slices
+  for (i = 1; i < 5; i++) {
+    voxels.resize(5);
+    sprintf(filename, "data/blurry/%d.bmp",i);
+    Image* image;
+    image = loadBMP(filename);
+    int width = image->width;
+    int height = image->height;
+    // need 3d array here.
+    int y = i;
+    int x, z;
+    int target=1;
+    for (x = 0; x < width; x+=sample) {
+      voxels[x].resize(width);
+      for (z = 0; z < height; z+=sample) {
+        voxels[x][z].resize(height);
+        unsigned char redcolor = 0;
+        // redcolor = (unsigned char)image ->pixels[3*(z * image->width + x)+2];
+        redcolor = (unsigned char)image ->pixels[3*(z * image->width + x)];
+        int red = (int)redcolor;
+        // time to outline the cheese
+        if (target) {
+          if (red<range) {
+            // glVertex3f(xb, yb, zb);
+            voxels[x][y][z]=1;
+            target=0;
+          }
+        }
+        else {
+          if (red>range) {
+            // glVertex3f(xb, yb, zb);
+            voxels[x][y][z]=0;
+            target=1;
+          }
+        }
+      }
+    }
+  }
+  
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(800, 800);
