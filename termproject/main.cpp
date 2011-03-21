1c1c29b842463af260ed26a3b0085d75dd60fc8d
Evan Travers <widgetwebworks@gmail.com>
2011-03-21 14:00:48 -0500

    we can see dots now!

--- termproject/main.cpp
+++ termproject/main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <fstream>
 
 #ifdef __APPLE__
 #include <OpenGL/OpenGL.h>
@@ -20,11 +21,18 @@ using namespace std;
 float _angle = 0.0f;
 float _angleLat = 00.0f;
 float _angleLon = 0.0f;
-float _walk = 40.0f;
+float _walk = 0.0f;
 float _stride = 400.0f;
 float _elevate = -400.0f;
 int width;
 int height;
+int data_length;
+struct GPScoord* coords;
+
+struct GPScoord {
+  float g_lat;
+  float g_long;
+};
 
 void cleanup() {
 }
@@ -138,13 +146,9 @@ void handleResize(int w, int h) {
 void drawScene() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glShadeModel(GL_SMOOTH);
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_LIGHTING);
-  glEnable(GL_LIGHT0);
-   
+  glColor3f(1.f, 1.f, 1.f);
   // Create light components
-  GLfloat ambientLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+  GLfloat ambientLight[] = { 0.5f, 0.5f, 0.5f, 1.0f };
   GLfloat diffuseLight[] = { 0.8f, 0.8f, 0.8, 1.0f };
   GLfloat specularLight[] = { 0.5f, 0.5f, 0.5f, 1.0f };
   GLfloat position[] = { -1.5f, 1.0f, -4.0f, 1.0f };
@@ -168,10 +172,10 @@ void drawScene() {
 
   glLoadIdentity();
 
-  glLoadIdentity();
   glPushMatrix();
   glScalef(0.0013f, 0.0013f, 0.0013f);
 
+  // printf("%f, %f, %f\n", _stride, _elevate, _walk);
   glTranslatef(-_stride, _elevate, _walk);
   glRotatef(_angleLat, 0.f, 1.f, 0.f);
   glRotatef(-_angleLon, 1.f, 0.f, 0.f);
@@ -182,6 +186,26 @@ void drawScene() {
     glTexCoord2d(1.0,1.0); glVertex2d(900.0,900.0);
     glTexCoord2d(0.0,1.0); glVertex2d(0.0,900.0);
   glEnd();
+
+
+  glColor3f(0.f, 0.f, 1.f);
+  // lets designate a point on the map
+  // ideally, we should read in points from a file
+
+
+  float x_pos = 0.f;
+  float y_pos = 0.f;
+  glPointSize(5.f);
+  glBegin(GL_POINTS);
+  int i;
+  for (i = 0; i < data_length; i++) {
+    // get the difference
+    x_pos = (-88.202949f - coords[i].g_long)/0.007f;
+    y_pos = (35.0080284f - coords[i].g_lat)/0.006f;
+    glVertex3f(243.f-x_pos, 843.f-y_pos, -50.f);
+  }
+  glEnd();
+
 	glutSwapBuffers();
 }
 
@@ -196,7 +220,31 @@ void update(int value) {
 }
 
 int main(int argc, char** argv) {
-  
+  // count the number of lines in the text file (number of locations)
+  FILE *f=fopen("data.txt","rb");
+  int c=0,b;while ((b=fgetc(f))!=EOF) c+=(b==10)?1:0;fseek(f,0,SEEK_SET);
+
+  data_length = c;
+  // make a structure in memory to hold the coords
+  coords = new struct GPScoord[c];
+
+  string val;
+  string val2;
+  struct GPScoord tmp;
+  fstream input;
+  input.open("data.txt");
+
+  // fill in the array
+  int counter = 0;
+  while (!input.eof()) {
+    input >> val;
+    input >> val2;
+    tmp.g_lat = atof(val.c_str());
+    tmp.g_long = atof(val2.c_str());
+    coords[counter] = tmp;
+    counter++;
+  }
+  input.close();
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
